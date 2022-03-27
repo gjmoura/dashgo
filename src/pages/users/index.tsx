e98b644eaddd491f0b8103777a25cc64da4a518a
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2022-03-27 17:56:44 -0300

    Componentização das componentes mais macros

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -80,6 +80,56 @@ export default function UserList() {
                   </Button>
                 </Td>
               </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Gustavo Jerônimo</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      gustavo.jeronimo@hotmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>27 de Março, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="16"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Gustavo Jerônimo</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      gustavo.jeronimo@hotmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>27 de Março, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="16"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
             </Tbody>
           </Table>
           <Pagination />
